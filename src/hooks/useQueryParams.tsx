d1602b1ba5aaef3eef052ea0b6fec0d72cc7779e
Gabacles <g.dalrovere@gmail.com>
2025-05-22 19:52:49 -0300

    Add useQueryParams and useUpdateQueryParam hooks for managing query parameters

---
+++ src/hooks/useQueryParams.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+export const useQueryParams = () => {
+  const searchParams = useSearchParams();
+
+  const actualParams = () => {
+    if (!window?.location) return "";
+
+    let searchParams = new URLSearchParams(window.location.search);
+    return searchParams.toString();
+  };
+
+  const append = (key: string, value: string) => {
+    if (!window?.location) return "";
+
+    let searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(key, value);
+
+    return searchParams.toString();
+  };
+
+  const buildQueryParams = (keys: string[]) => {
+    let query: string = "";
+
+    for (const key of keys) {
+      const value = searchParams.get(key);
+      if (value) {
+        const sufix = !!query ? "&" : "?";
+        query += `${sufix}${key}=${value}`;
+      }
+    }
+
+    const sufix = !!query ? "&" : "?";
+
+    if (!query.includes("size")) query += sufix + `size=${5}`;
+
+    return query;
+  };
+
+  return { append, actualParams, buildQueryParams };
+};
