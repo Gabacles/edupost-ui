1b857f13fdd8023edf5222c104afa6b5f3e5fce5
Gabacles <g.dalrovere@gmail.com>
2025-05-22 17:37:33 -0300

    Add random images for post display

---
+++ src/styles/randomImages.ts
@@ -0,0 +1,43 @@
+import Img1 from "@/assets/postImages/random-post-1.png";
+import Img2 from "@/assets/postImages/random-post-2.png";
+import Img3 from "@/assets/postImages/random-post-3.png";
+import Img4 from "@/assets/postImages/random-post-4.png";
+import Img5 from "@/assets/postImages/random-post-5.png";
+import Img6 from "@/assets/postImages/random-post-6.png";
+import Img7 from "@/assets/postImages/random-post-7.png";
+import Img8 from "@/assets/postImages/random-post-8.png";
+import Img9 from "@/assets/postImages/random-post-9.png";
+import Img10 from "@/assets/postImages/random-post-10.png";
+import Img11 from "@/assets/postImages/random-post-11.png";
+import Img12 from "@/assets/postImages/random-post-12.png";
+import Img13 from "@/assets/postImages/random-post-13.png";
+import Img14 from "@/assets/postImages/random-post-14.png";
+import Img15 from "@/assets/postImages/random-post-15.png";
+import Img16 from "@/assets/postImages/random-post-16.png";
+import Img17 from "@/assets/postImages/random-post-17.png";
+import Img18 from "@/assets/postImages/random-post-18.png";
+import Img19 from "@/assets/postImages/random-post-19.png";
+import Img20 from "@/assets/postImages/random-post-20.png";
+
+export const randomImages = [
+  Img1,
+  Img2,
+  Img3,
+  Img4,
+  Img5,
+  Img6,
+  Img7,
+  Img8,
+  Img9,
+  Img10,
+  Img11,
+  Img12,
+  Img13,
+  Img14,
+  Img15,
+  Img16,
+  Img17,
+  Img18,
+  Img19,
+  Img20,
+];
