24ef4ea892a96a1102e981aa1f4748c8cc4c966c
Gabacles <g.dalrovere@gmail.com>
2025-05-22 17:33:08 -0300

    Add User type definition with username field to user model

--- src/models/types/user.ts
+++ src/models/types/user.ts
@@ -2,3 +2,11 @@ export enum UserRoles {
   TEACHER = "TEACHER",
   STUDENT = "STUDENT",
 }
+
+export type User = {
+  id: number;
+  email: string;
+  roles: UserRoles;
+  name: string;
+  username: string;
+};
