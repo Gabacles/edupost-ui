08022752655dcdaa0296653796c09186cad38dc3
Gabacles <g.dalrovere@gmail.com>
2025-05-22 19:54:57 -0300

    Refactor GET handler in posts route to include error handling and dynamic query parameters

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,24 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const authToken = request.cookies.get("access_token")?.value;
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVICES_BASE_URL}/posts`,
-    {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
+  try {
+    const authToken = request.cookies.get("access_token")?.value;
+    if (!authToken) {
+      return NextResponse.json(
+        { error: "Token de autenticação ausente." },
+        { status: 401 }
+      );
     }
-  );
 
-  if (!res.ok) {
+    const searchParams = new URLSearchParams(request.nextUrl.search);
+    const query = searchParams && "?" + searchParams.toString();
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVICES_BASE_URL}/posts${query}`,
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      return NextResponse.json(data, { status: res.status });
+    }
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Erro ao buscar posts:", error);
     return NextResponse.json(
-      { error: "Erro ao buscar posts" },
-      { status: res.status }
+      { error: "Erro inesperado ao buscar posts." },
+      { status: 500 }
     );
   }
-
-  const posts = await res.json();
-  return NextResponse.json(posts);
 }
