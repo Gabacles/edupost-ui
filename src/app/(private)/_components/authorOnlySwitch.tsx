7f06a570c89d8049455e0d1cfa38302d8be259dd
Gabacles <g.dalrovere@gmail.com>
2025-05-23 00:47:27 -0300

    Add AuthorOnlySwitch component for toggling visibility of author's posts

---
+++ src/app/(private)/_components/authorOnlySwitch.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { Switch } from "@/components/shared/switch";
+import { useUserStore } from "@/hooks/user/useUserStore";
+import { useUpdateQueryParam } from "@/hooks/useUpdateQueryParam";
+import { useRouter, useSearchParams } from "next/navigation";
+import { UserRoles } from "@/models/types/user";
+
+export const AuthorOnlySwitch = () => {
+  const [authorId, setAuthorId] = useState<number | undefined>();
+  const [authorRole, setAuthorRole] = useState<UserRoles | undefined>(
+    UserRoles.STUDENT
+  );
+  const { getUserData } = useUserStore();
+
+  useEffect(() => {
+    const user = getUserData();
+    setAuthorId(user?.id);
+    setAuthorRole(user?.roles);
+  }, [getUserData]);
+
+  const router = useRouter();
+  const params = useSearchParams();
+  const { updateQuery } = useUpdateQueryParam();
+
+  if (authorRole !== UserRoles.TEACHER) return null;
+
+  const isAuthorOnly = params.get("authorId") === authorId?.toString();
+
+  const handleChange = (checked: boolean) => {
+    const newQuery = updateQuery(
+      "authorId",
+      checked ? authorId?.toString()! : "",
+      ["page"]
+    );
+    router.push(newQuery);
+  };
+
+  return (
+    <div className="flex items-center gap-2 cursor-pointer">
+      <Switch
+        id="userOnly"
+        className="cursor-pointer"
+        checked={isAuthorOnly}
+        onCheckedChange={handleChange}
+      />
+      <label htmlFor="userOnly" className="text-sm cursor-pointer">
+        Somente meus posts
+      </label>
+    </div>
+  );
+};
