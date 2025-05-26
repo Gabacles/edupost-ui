8b0f6d1507f65aed85f977b56e4e681654bdb502
Gabacles <g.dalrovere@gmail.com>
2025-05-26 11:50:21 -0300

    Replace Suspense fallback with PostListSkeleton for improved loading experience

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,11 +1,12 @@
 import { Suspense } from "react";
 import { PostList } from "./_components/postList";
+import { PostListSkeleton } from "./_components/postListSkeleton";
 
 export default function Home() {
   return (
     <main>
       <div className="justify-center items-center flex">
-        <Suspense fallback={<div>Carregando lista de posts...</div>}>
+        <Suspense fallback={<PostListSkeleton />}>
           <PostList />
         </Suspense>
       </div>
