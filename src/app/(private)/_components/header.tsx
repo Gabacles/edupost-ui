604b473fead2e7cee1e6f73c61cf1cd208702961
Gabacles <g.dalrovere@gmail.com>
2025-05-26 20:33:17 -0300

    Hide elements on smaller screens in Header and UserMenu components for improved responsiveness

--- src/app/(private)/_components/header.tsx
+++ src/app/(private)/_components/header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
     <div className="flex items-center justify-between w-full min-h-16 bg-white shadow-edu-shadow px-12 border border-b-edupost-blue-primary">
       <Link href="/" className="flex items-center">
         <Image src={EduPostLogo} alt="EduPost Logo" width={33} className="" />
-        <span className="text-edupost-blue font-semibold">edupost</span>
+        <span className="text-edupost-blue font-semibold hidden md:inline-block">edupost</span>
       </Link>
 
       <UserMenu />
